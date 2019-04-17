a68049e0da8728a1fb9acadf28cb742691d1e191
alphamarshan <andrewmarsh42@outlook.com>
2019-04-17 13:59:36 -0400

    working now

--- word_of_the_day2.py
+++ word_of_the_day2.py
@@ -3,28 +3,31 @@ import requests
 import random
 import json
 
-# Initiate the input which will be used as the Word of the Day
-user_input = input("Enter a word for the dictionary to define: ")
+#Opens GRE word list and reads lines into a list
+file = open("gre_wordlist.txt")
+word_list = file.read().split()
+file.close()
 
-# Open and read the list of English words
-words_file = open("words.txt", "r")
-all_words = words_file.read()
-words_file.close()
+# Organizes the list and generates a random word from within the list
+new_word_list = [x[:-1] for x in word_list]
+del new_word_list[:10]
+list_length = len(new_word_list)
+word_id = random.randint(0, list_length)
+word = new_word_list[word_id]
 
-# Variables
+# API Variables
 app_id = "2f5596aa"
 app_key = "cb0e7fe2e343054d8bd14743f44f5b6c"
 language = "en"
 region = "us"
-word_id = str(user_input)
 
 # Create a URL with API keys to access the Oxford Dictionary API
 base_url = "https://od-api.oxforddictionaries.com:443/api/v1" 
-appended_url = "/entries/" + language + "/" + word_id.lower() + "/regions=" + region
+appended_url = "/entries/" + language + "/" + word + "/regions=" + region
 requested_url = base_url + appended_url
 
 # Create the GET request for the API
-r = requests.get(requested_url, headers = {"app_id": app_id, "app_key" : app_key})
+r = requests.get(requested_url, headers={"app_id": app_id, "app_key": app_key})
 
 # Status code handling
 if r.status_code == 200:
@@ -32,11 +35,11 @@ if r.status_code == 200:
 else:
     print("Error! Something went wrong.")
 
-# Handle/parse the JSON that returns
+# # Handle/parse the JSON that returns
 parsed_text = json.loads(r.text)
-# print(json.dumps(parsed_text, indent=2, sort_keys=True))
+# # print(json.dumps(parsed_text, indent=2, sort_keys=True))
 
 results_id = parsed_text["results"][0]["id"].capitalize()
 results_definition = parsed_text["results"][0]["lexicalEntries"][0]["entries"][0]["senses"][0]["definitions"][0].capitalize()
 
-print(f"The word of the day is: \n\n{results_id}\n{results_definition}\n\n")
+print(f"The word of the day is: \n\n{results_id}\n{results_definition}\n\n\n\n")
